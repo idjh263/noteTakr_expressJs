2fbbbd149cc3b90233e36640cc2a4308b58479eb
Irene Hipolito <irenehipolito@Irenes-MacBook-Air.local>
2022-01-30 22:33:24 -0500

    note-taker added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,29 @@
-const router = require('express').Router();
-const store = require('./../db/store');
+const router = require("express").Router();
+const store = require("./../db/store");
 
-router.get('/notes', (req, res) => {
+// ==========
+// ROUTES
+// ==========
+
+router.get("/notes", function (req, res) {
     store
-    .getNotes()
-    .then((notes) => {
-        return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err))
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
 });
 
-router.post('/notes', (req, res) => {
+router.post("/notes", function (req, res) {
     store
-    .addNote(req.body)
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
+        .addNote(req.body)
+        .then((notes) => res.json(notes))
+        .catch(err => res.status(500).json(err))
 });
 
-router.delete('/notes/:id', (req, res) => {
+router.delete("/notes/:title", function (req, res) {
     store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
+        .deleteNotes(req.params.title)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
 });
 
 module.exports = router;
